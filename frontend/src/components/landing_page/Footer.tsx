9cb1a8c9f280354a6f048bcb49f41d252e8bb9a7
gbstorey <gbstorey@asu.edu>
2022-12-23 16:08:58 -0700

    Fix footer CSS

--- frontend/src/components/landing_page/Footer.tsx
+++ frontend/src/components/landing_page/Footer.tsx
@@ -5,12 +5,12 @@ const Footer = () => {
     return (
         <div className={classes.footer}>
             <div className={classes.footerContent}>
-                <a href={"/"}><img src={"/images/cat_wiki_logo.svg"} alt={"Cat Wiki Logo"}/></a>
+                <a href={"/"}><img className={classes.footerLogo} src="/images/cat_wiki_logo.svg" alt={"Cat Wiki Logo"}/></a>
                 <div>
-                    <i className="fa-regular fa-copyright"></i>
-                    <p>created by </p>
-                    <p><a href={"https://github.com/gbstorey"}>garrett storey</a></p>
-                    <p> - devChallenge.io 2021</p>
+                    <i className={`fa-regular fa-copyright ${classes.footerIcon}`}></i>
+                    <p className={classes.footerText}>created by </p>
+                    <p className={classes.footerText}><a href={"https://github.com/gbstorey"} className={classes.footerLink}>garrett storey</a></p>
+                    <p className={classes.footerText}> - devChallenge.io 2021</p>
                 </div>
             </div>
         </div>
--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,9 +1,7 @@
 import './Root.module.css'
-import RootLayout from "../components/UI/RootLayout";
 import Hero from "../components/landing_page/Hero";
 import Recommendations from "../components/landing_page/Recommendations";
 import AboutCats from "../components/landing_page/AboutCats";
-import Footer from "../components/landing_page/Footer";
 import classes from "./Root.module.css"
 
 function Root() {
@@ -12,7 +10,7 @@ function Root() {
     <div className={classes.centralFrame}>
         <Hero />
         <Recommendations />
-        <AboutCats />s
+        <AboutCats />
     </div>
   )
 }
